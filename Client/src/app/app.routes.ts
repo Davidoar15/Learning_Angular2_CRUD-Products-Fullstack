2b0de64582c6f131f62f8ad823af2c8521133461
Davidoar15 <davidoar15@gmail.com>
2024-01-09 23:40:17 -0300

    learning-routing

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductsComponent } from './components/add-edit-products/add-edit-products.component';
+
+export const routes: Routes = [
+    { path: "", component: ListProductsComponent },
+    { path: "add", component: AddEditProductsComponent },
+    { path: "edit/:id", component: AddEditProductsComponent },
+    { path: "**", redirectTo: "", pathMatch: "full" }
+];
--- Client/src/app/components/list-products/list-products.component.ts
+++ Client/src/app/components/list-products/list-products.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { Product } from '../../interfaces/product';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list-products',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
 export class ListProductsComponent {
   listProducts: Product[] = [
     {
+      id: 1,
       name: "Coca-Cola",
       description: "Refresco",
       price: 50,
       stock: 100
     },
     {
+      id: 2,
       name: "Quilmes",
       description: "Cerveza",
       price: 20,
