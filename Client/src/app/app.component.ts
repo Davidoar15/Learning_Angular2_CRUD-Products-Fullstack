f52d51148edd545ee7b4f9c773581cd1bc4eb88f
Davidoar15 <davidoar15@gmail.com>
2024-01-10 12:49:22 -0300

    add/add-edit-validations

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListProductsComponent } from './components/list-products/list-products.component';
 
--- Client/src/app/components/add-edit-products/add-edit-products.component.ts
+++ Client/src/app/components/add-edit-products/add-edit-products.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-add-edit-products',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './add-edit-products.component.html',
   styleUrl: './add-edit-products.component.css'
 })
 export class AddEditProductsComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [null, Validators.required],
+      stock: [null, Validators.required]
+    })
+  };
+
+  addProduct() {
+    // console.log(this.form.value.name);
+    const product: Product = {
+      name: this.form.value.name,
+      description: this.form.value.description,
+      price: this.form.value.price,
+      stock: this.form.value.stock
+    };
+    console.log(product);
+  };
 }
