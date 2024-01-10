9aab10575ac14623b362f433b79edababc9b89dc
Davidoar15 <davidoar15@gmail.com>
2024-01-10 18:00:00 -0300

    add/CRUD-products-Back_DB

--- Server/src/controllers/products.ts
+++ Server/src/controllers/products.ts
@@ -3,15 +3,15 @@ import Product from "../models/product";
 
 export const getProducts = async (req: Request, res: Response) => {
     try {
-        const listProducts = await Product.findAll()
+        const listProducts = await Product.findAll();
 
         listProducts 
         ? res.status(200).json(listProducts)
         : res.status(404).send("Products Not Found")
     } catch (error) {
-        console.log("GET PRODUCTS", error)
+        console.log("GET PRODUCTS", error);
         res.status(500).json({ msg: "Internal Server Error" });
-    }
+    };
 };
 
 export const getProduct = async (req: Request, res: Response) => {
@@ -24,35 +24,64 @@ export const getProduct = async (req: Request, res: Response) => {
         : res.status(404).send(`Product ${id} Not Found`)
     } catch (error) {
         console.log("GET PRODUCT", error);
-        res.status(500).json({ msg: "Internal Server Error" })
+        res.status(500).json({ msg: "Internal Server Error" });
+    };
+};
+
+export const postProduct = async (req: Request, res: Response) => {
+    try {
+        const { body } = req;
+    
+        await Product.create(body);
+    
+        res.status(200).json({
+            msg: "Product Added Correctly",
+            product: body
+        });
+    } catch (error) {
+        console.log("POST PRODUCT", error);
+        res.status(500).json({ msg: "Internal Server Error" });
+    };
+};
+
+export const putProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { body } = req;
+    
+        const product = await Product.findByPk(id);
+    
+        if(product) {
+            await product.update(body);
+            await res.status(200).json({
+                msg: "Product Updated Correctly",
+                product
+            })
+        } else {
+            res.status(404).send(`Product ${id} Not Found`);
+        }
+    } catch (error) {
+        console.log("UPDATE PRODUCT", error);
+        res.status(500).json({ msg: "Internal Server Error" });
     }
 };
 
-export const postProduct = (req: Request, res: Response) => {
-    const { body } = req;
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByPk(id);
 
-    res.status(200).json({
-        msg: "POST Product",
-        body
-    });
-};
-
-export const putProduct = (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { body } = req;
-
-    res.status(200).json({
-        msg: "UPDATE Product",
-        id,
-        body
-    });
-};
-
-export const deleteProduct = (req: Request, res: Response) => {
-    const { id } = req.params;
-
-    res.status(200).json({
-        msg: "DELETE Product",
-        id: id
-    });
+        if (!product) {
+            res.status(404).send(`Product ${id} Not Found`);
+        } else {
+            await product.destroy();
+            res.status(200).json({
+                msg: "Product Deleted Correctly",
+                product
+            });
+        };
+    } catch (error) {
+        console.log("DELETE PRODUCT", error);
+        res.status(500).json({ msg: "Internal Server Error" });
+    };
 };
