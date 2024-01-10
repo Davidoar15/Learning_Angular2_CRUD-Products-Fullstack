fbe23adbcf3f27d746aed41e16bc97a35bdbbf2b
Davidoar15 <davidoar15@gmail.com>
2024-01-09 21:15:05 -0300

    add/components-services-interfaces

---
+++ Client/src/app/components/add-edit-products/add-edit-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-edit-products.component.html',
+  styleUrl: './add-edit-products.component.css'
+})
+export class AddEditProductsComponent {
+
+}
